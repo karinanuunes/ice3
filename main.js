ee1219a3b11969fd206f3a66080fbffaf122e7b4
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-01-11 11:48:55 -0300

    add function contato

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const catalogo = document.getElementById("catalogo");
 const ordem = document.getElementById("ordem");
 const about = document.getElementById("about");
 const tabela = document.getElementById("tabela");
+const contato = document.getElementById("contato");
 
 function abrirSobre() {
   catalogo.style.display = "none";
   ordem.style.display = "none";
   tabela.style.display = "none";
+  contato.style.display = "none";
 
   about.style.display = "flex";
 }
@@ -15,6 +17,16 @@ function abrirTabela() {
   catalogo.style.display = "none";
   ordem.style.display = "none";
   about.style.display = "none";
+  contato.style.display = "none";
 
   tabela.style.display = "flex";
+}
+
+function abrirContato() {
+  catalogo.style.display = "none";
+  ordem.style.display = "none";
+  about.style.display = "none";
+  tabela.style.display = "none";
+
+  contato.style.display = "flex";
 }
