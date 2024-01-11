c754de1f4df8790165577dc31497cae996924976
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-01-11 12:35:57 -0300

    add cadastro

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const catalogo = document.getElementById("catalogo");
 const ordem = document.getElementById("ordem");
+const cadastro = document.getElementById("cadastro");
 const about = document.getElementById("about");
 const tabela = document.getElementById("tabela");
 const contato = document.getElementById("contato");
@@ -7,6 +8,7 @@ const contato = document.getElementById("contato");
 function abrirSobre() {
   catalogo.style.display = "none";
   ordem.style.display = "none";
+  cadastro.style.display = "none";
   tabela.style.display = "none";
   contato.style.display = "none";
 
@@ -16,6 +18,7 @@ function abrirSobre() {
 function abrirTabela() {
   catalogo.style.display = "none";
   ordem.style.display = "none";
+  cadastro.style.display = "none";
   about.style.display = "none";
   contato.style.display = "none";
 
@@ -25,8 +28,19 @@ function abrirTabela() {
 function abrirContato() {
   catalogo.style.display = "none";
   ordem.style.display = "none";
+  cadastro.style.display = "none";
   about.style.display = "none";
   tabela.style.display = "none";
 
   contato.style.display = "flex";
+}
+
+function abrirCadastro() {
+  catalogo.style.display = "none";
+  ordem.style.display = "none";
+  about.style.display = "none";
+  tabela.style.display = "none";
+  contato.style.display = "none";
+
+  cadastro.style.display = "flex";
 }
